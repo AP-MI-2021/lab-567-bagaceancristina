d2a340de22f9371f64bbe598d2ef0f9a80919c8d
bagaceancristina <bagacean_cristina@yahoo.com>
2021-11-07 16:56:11 +0200

    1.5

--- UserInterface/console.py
+++ UserInterface/console.py
@@ -2,6 +2,7 @@ from domain.cheltuiala2 import get_cheltuiala
 from logic.crud import create, update, delete
 from logic.majorare_cheltuieli import majorare
 from logic.max_cheltuieli_tip import get_max_chelt_tip
+from logic.ordonare_pret import ordonare_suma
 from logic.stergere_cheltuieli import del_chelt_apartament
 
 
@@ -10,6 +11,7 @@ def show_menu():
     print('2.Stergerea cheltuielilor unui apartament dat.')
     print('3.Adunarea unei valori la toate cheltuielile dintr-o dată citită.')
     print('4.Afisarea celei mai scumpe cheltuieli pentru fiecare tip de cheltuiala.')
+    print('5.Ordonarea cheltuielilor in functie de suma.')
     print('x.Exit')
 
 
@@ -55,26 +57,38 @@ def handle_delete(cheltuieli):
 
 
 def handle_del_chelt_ap(cheltuieli):
-    apartament = int(input("Introduceti numarul apartamentului a caror cheltuieli doriti sa le stergeti : "))
-    result = del_chelt_apartament(cheltuieli, apartament)
-    print('Stergerea a fost efectuata.')
-    print(result)
-    return result
+    try:
+        apartament = int(input("Introduceti numarul apartamentului a caror cheltuieli doriti sa le stergeti : "))
+        result = del_chelt_apartament(cheltuieli, apartament)
+        print('Stergerea a fost efectuata.')
+        print(result)
+        return result
+    except ValueError as ve:
+        print('Eroare: ', ve)
+    return cheltuieli
 
 
 def handle_majorare(cheltuieli):
-    data = input("Introduceti data in care doriti sa majorati cheltuielile :")
-    suma = float(input("Introduceti suma cu care doriti sa majorati cheltuielile :"))
-    result = majorare(cheltuieli, data, suma)
-    print('Majorarea a fost efectuata.')
-    print(result)
-    return result
+    try:
+        data = input("Introduceti data in care doriti sa majorati cheltuielile :")
+        suma = float(input("Introduceti suma cu care doriti sa majorati cheltuielile :"))
+        result = majorare(cheltuieli, data, suma)
+        print('Majorarea a fost efectuata.')
+        print(result)
+        return result
+    except ValueError as ve:
+        print('Eroare : ', ve)
+    return cheltuieli
 
 
 def handle_max_chelt_tip(cheltuieli):
     result = get_max_chelt_tip(cheltuieli)
     print(result)
 
+
+def handle_ordonare(cheltuieli):
+    result = ordonare_suma(cheltuieli)
+    handle_show_all(result)
 
 def handle_crud(cheltuieli):
     while True:
@@ -112,6 +126,8 @@ def run_ui(cheltuieli):
             cheltuieli = handle_majorare(cheltuieli)
         elif optiune == '4':
             handle_max_chelt_tip(cheltuieli)
+        elif optiune == '5':
+            handle_ordonare(cheltuieli)
         elif optiune == 'x':
             break
         else:
