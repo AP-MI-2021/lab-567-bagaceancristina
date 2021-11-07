0c728f09ffc2733a041140451d06d69cce3677d4
bagaceancristina <bagacean_cristina@yahoo.com>
2021-11-07 15:39:48 +0200

    1.4

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from UserInterface.console import run_ui
 from UserInterface.console2 import console2
 from tests.test_crud import test_crud
 from tests.test_majorare import test_majorare
+from tests.test_max_chelt_tip import test_max
 from tests.test_stergere_cheltuieli import test_stergere_cheltuieli
 
 
@@ -10,10 +11,12 @@ def main():
     cheltuieli = run_ui(cheltuieli)
     #cheltuieli = console2(cheltuieli)
 
+
 if __name__ == '__main__':
     test_crud()
     test_majorare()
     test_stergere_cheltuieli()
+    test_max()
     main()
     #console2()
 
---
+++ tests/test_max_chelt_tip.py
@@ -0,0 +1,17 @@
+from domain.cheltuiala2 import creeaza_cheltuiala
+from logic.crud import read
+from logic.max_cheltuieli_tip import get_max_chelt_tip
+
+
+def get_data():
+    return[
+        creeaza_cheltuiala(1, 1, 12.6, '08.12.2012', 'gaz'),
+        creeaza_cheltuiala(2, 4, 92.6, '22.12.2002', 'gaz'),
+        creeaza_cheltuiala(3, 7, 102.8, '02.01.2002', 'apa'),
+        creeaza_cheltuiala(4, 9, 77, '19.08.2002', 'lumina')
+    ]
+
+def test_max():
+    cheltuieli = get_data()
+    result = get_max_chelt_tip(cheltuieli)
+    assert len(cheltuieli) != len(result)
